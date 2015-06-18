b5a9d1aacb24bddb156ba3f1f0acdab5d11cee83
Prajwal <prajwal19@gmail.com>
2015-06-17 22:49:25 -0500

    first commit

---
+++ app/src/main/java/com/prajwal/interview/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.prajwal.interview.app;
+
+/**
+ * Created by Hp on 17-06-2015.
+ */
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.1/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.1/android_login_api/";
+}
